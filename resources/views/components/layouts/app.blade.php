6da40819ed1a62f5d7f4e23f75e778e883a37117
Mockelyn Maxime <mmockelyn@vortechstudio.fr>
2024-01-11 17:31:49 +0100

    :art: refactor: Met à jour les liens de navigation et ajoute un nom à la route d'accueil
    VL-4-definir-la-charte-graphique

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -72,7 +72,7 @@
 
                                         <div class="menu menu-rounded menu-column menu-lg-row menu-active-bg menu-title-gray-700 menu-state-primary menu-arrow-gray-500 fw-bold my-5 my-lg-0 align-items-stretch px-2 px-lg-0 fs-3" id="#header_menu" data-kt-menu="true">
                                             <div class="menu-item me-0 me-lg-2">
-                                                <a href="" class="menu-link active">Accueil</a>
+                                                <a href="" class="menu-link {{ request()->route()->named('home') ? 'active' : '' }}" wire:navigate>Accueil</a>
                                             </div>
                                             <div class="menu-item menu-here-bg menu-lg-down-accordion me-0 me-lg-2" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-placement="bottom-start">
                                                 <span class="menu-link py-3">
@@ -83,7 +83,7 @@
                                                     <div class="menu-state-bg menu-extended overflow-hidden overflow-lg-visible" data-kt-menu-dismiss="true">
                                                         @foreach($cercles as $cercle)
                                                             <div class="menu-item p-0 m-0">
-                                                                <a href="" class="menu-link">
+                                                                <a href="" class="menu-link" wire:navigate>
                                                                     <span class="menu-custom-icon d-flex flex-center flex-shrink-0 rounded w-40px h-40px me-3">
                                                                         <img src="{{ $cercle->image_icon }}" alt="" class="w-40px h-40px rounded-4">
                                                                     </span>
