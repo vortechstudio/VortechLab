0741d0b490ab0450f65d1271d3bc8cda1a3f16e0
Mockelyn Maxime <mmockelyn@vortechstudio.fr>
2024-01-19 20:15:43 +0100

    fr_fr: Ajouter le nouveau fichier LoginController.php. Modifier la fonction dans User.php pour utiliser la session plutôt que les cookies pour récupérer l'ID utilisateur.

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
+
+class LoginController extends Controller
+{
+    use LivewireAlert;
+    public function login(Request $request)
+    {
+        if($request->get('logged') == true) {
+            \Session::push('user_uuid', $request->get('user_uuid'));
+            flash()->addSuccess("Connexion effectuer avec succès", "Bienvenue $request->name");
+            return redirect()->intended();
+        } else {
+            flash()->addError("Connexion échouer", "Veuillez vérifier vos informations de connexion");
+            return redirect()->intended();
+        }
+    }
+}
